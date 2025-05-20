e4c0cde02a251b3d84939451bf7f5dd86fec5bf5
Sandeep Suthar <ss.suthar.004@gmail.com>
2025-05-20 18:08:41 +0530

    upgrade

---
+++ src/constant/__init__.py
@@ -0,0 +1,13 @@
+import os
+from  datetime import datetime
+
+DATABASE_NAME = "SAMPLE_PRO"
+
+COLLECTION_NAME = "PRO_DATA"
+
+MONFODB_URL_KEY = "MONGODB_URL"
+
+PIPELINE_NAME: str = "SRCPIPE"
+ARTIFACT_DIR: str = "artifact"
+
+MODEL_FILE_NAME = "model.pkl"
