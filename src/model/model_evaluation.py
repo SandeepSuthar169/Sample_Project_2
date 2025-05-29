fa221c45cb76c28b8e626b214e8efb4f5ac8d5d2
Sandeep Suthar <ss.suthar.004@gmail.com>
2025-05-29 16:37:30 +0530

    model evaluation added

---
+++ src/model/model_evaluation.py
@@ -0,0 +1,23 @@
+import pandas as pd
+import json
+import os
+import pickle
+from sklearn.metrics import accuracy_score, f1_score, recall_score, precision_score
+
+test_data = pd.read_csv("./data/processed/test_preprocessing.csv")
+
+X_test = test_data.drop(columns=['case_status'])
+y_test = test_data['case_status']
+
+pipeline = pickle.load(open('model.pkl', "rb"))
+
+y_pred = pipeline.predict(X_test)
+
+accuracy = accuracy_score(y_test, y_pred)
+
+metrics= {
+    'accuracy': accuracy
+}
+
+with open('metrics.json', 'w') as file:
+    json.dump(metrics, file, indent=4)
