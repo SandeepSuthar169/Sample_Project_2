70370c8741b4d97bf07aa49f871df5ab4e69e9ba
Sandeep Suthar <ss.suthar.004@gmail.com>
2025-06-16 19:21:23 +0530

    Update data_ingestion.py

--- src/data/data_ingestion.py
+++ src/data/data_ingestion.py
@@ -8,17 +8,13 @@ from sklearn.model_selection import train_test_split
 
 logger = logging.getLogger('data_ingestion')
 logger.setLevel('DEBUG')
-
 console_handler = logging.StreamHandler()
 console_handler.setLevel('DEBUG')
-
 file_handler = logging.FileHandler('errors.log')
 file_handler.setLevel('DEBUG')
-
 formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
 console_handler.setFormatter(formatter)
 file_handler.setFormatter(formatter)
-
 logger.addHandler(console_handler)
 logger.addHandler(file_handler)
 
